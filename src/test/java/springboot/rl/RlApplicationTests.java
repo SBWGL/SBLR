fca16b4c2ed3eec04910af5729e376ed6cab1a77
lehehwan <rdsa257@naver.com>
2021-08-20 17:59:10 +0900

    init

---
+++ src/test/java/springboot/rl/RlApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.rl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
