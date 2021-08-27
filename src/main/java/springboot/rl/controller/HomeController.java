45cccc4f49ecad28f2afe96c91bf9fbf87e00be1
lehehwan <rdsa257@naver.com>
2021-08-27 17:25:24 +0900

    google map

--- src/main/java/springboot/rl/controller/HomeController.java
+++ src/main/java/springboot/rl/controller/HomeController.java
@@ -12,4 +12,9 @@ public class HomeController {
     public String home(){
         return "index";
     }
+
+    @GetMapping("/contact")
+    public String contact(){
+        return "contact";
+    }
 }
